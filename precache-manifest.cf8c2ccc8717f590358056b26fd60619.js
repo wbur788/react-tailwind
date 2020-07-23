dd89466dc590d1e96b6be6ac22174775c2aa1f4f
Wannachai <wbur788@aucklanduni.ac.nz>
2020-07-23 21:17:00 +1200

    Updates

---
+++ precache-manifest.cf8c2ccc8717f590358056b26fd60619.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fdd58b9b0870ff1f24fc51b9a9bbf63",
+    "url": "/react-tailwind/index.html"
+  },
+  {
+    "revision": "fd3f48cd7c35e499b401",
+    "url": "/react-tailwind/static/css/main.3f8e1b76.chunk.css"
+  },
+  {
+    "revision": "9477b05828f876efe41e",
+    "url": "/react-tailwind/static/js/2.7038155f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tailwind/static/js/2.7038155f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd3f48cd7c35e499b401",
+    "url": "/react-tailwind/static/js/main.3c83d680.chunk.js"
+  },
+  {
+    "revision": "8edfd4cf7573c2f74d7c",
+    "url": "/react-tailwind/static/js/runtime-main.ef017eee.js"
+  }
+]);
